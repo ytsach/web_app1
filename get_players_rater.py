5cb4dafa2044d8a6a953adc41319da33e6e786f6
tyanay <ytsach@gmail.com>
2021-02-06 13:47:59 +0200

    fix 0 in rater

--- get_players_rater.py
+++ get_players_rater.py
@@ -14,7 +14,12 @@ def get_player_rating_league(player_name=None, data=get_statistic()[2],scope='To
     duplicate = all_data_to_rate.groupby('Player').filter(lambda x: len(x) > 2).drop_duplicates(subset='Player')
     all_data_to_rate = pd.concat([all_data_to_rate, duplicate])
     for index, row in all_data_to_rate.iterrows():
-        all_data_to_rate.loc[index, 'Analyzer  {}'.format(scope)] = rater(player_name=row['Player'], data=data)[0]
+
+        rate = rater(player_name=row['Player'], data=data)
+        if rate == 0:
+            all_data_to_rate.loc[index, 'Analyzer  {}'.format(scope)] = 0
+        else:
+            all_data_to_rate.loc[index, 'Analyzer  {}'.format(scope)] = rate[0]
 
     if player_name:
         all_data_to_rate = all_data_to_rate.sort_values('Analyzer  {}'.format(scope), ascending=False).reset_index()
